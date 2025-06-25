791a4ac4ee3cdf4dc36702c9c9a552df3b94eb97
kaguramaru <92495243+kagura-maru@users.noreply.github.com>
2025-06-25 21:45:59 +0800

    Update and rename NetworkConfigurer.ps1 to NetworkTroubleshooter.ps1

--- NetworkTroubleshooter.ps1
+++ NetworkTroubleshooter.ps1
@@ -1,13 +1,8 @@
 # Powershell Script
+# To run this in powershell script. i.e: powershell .\NetworkTroubleshooter.ps1
 
 Echo (Get-NetIPAddress | Where-Object "AddressFamily" -eq "IPv6" | Select-Object "IPAddress").count
 
 # Developer Note: Send this to the client and ask them to run this to to see if they can access internet from their end.
 # cmd /c curl -s https://pastebin.com/raw/7MJJhZf8
-
-
-
-
-
-
 
